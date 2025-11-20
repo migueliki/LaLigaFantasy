26011628f8aa089329a6e9d2d101dd1546f12ede
miguel <miguel2006ngl@gmail.com>
2025-11-20 14:36:11 +0100

    estrutura php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$pages = $_GET ['pages'] ?? 'inicio';
+$list_pages = ['inicio', 'lista_jugadores'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista De Jugadores</title>
+    
+</head>
+<body>
+    
+
+
+
+
+
+<?php
+
+
+
+?>
+
+
+</body>
+</html>

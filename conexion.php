ca59c76c10b9e033286e6df1b1f1675e43d4a2f3
miguel <miguel2006ngl@gmail.com>
2025-11-21 01:39:08 +0100

    prueba de login y register a base de datos

--- conexion.php
+++ conexion.php
@@ -1,4 +1,4 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=fantasy", "root", "furbo"); // conexion a la base de datos
+$pdo = new PDO("mysql:host=localhost;dbname=fantasy", "root", ""); // conexion a la base de datos
 
 ?>
--- register.php
+++ register.php
@@ -4,12 +4,13 @@ include 'conexion.php';
 
 // Obtener datos del formulario
 $usuario = $_POST['usuario'];
+$email = $_POST['email'];
 $contraseña = $_POST['contraseña'];
 
 // Buscar usuario
-$sql = "INSERT INTO register (usuario,contraseña) VALUES (?, ?)"; // los ? son parametros que se van a reemplazar despues para prevenir injeccion sql
+$sql = "INSERT INTO register (usuario,email,contraseña) VALUES (?, ?, ?)"; // los ? son parametros que se van a reemplazar despues para prevenir injeccion sql
 $stmt = $pdo->prepare($sql); // le esta diciendo que use la conexion $pdo para preparar el insert sql
-$stmt->execute([$usuario, $contraseña]); // ejecuta el insert con el parametro usuario y contraseña reemplazando los ? 
+$stmt->execute([$usuario, $email, $contraseña]); // ejecuta el insert con el parametro usuario y contraseña reemplazando los ? 
 
 echo "Usuario registrado exitosamente";
 

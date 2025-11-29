51732ba37879e23caf0644ce6b7310a5c19ab80b
miguel <miguel2006ngl@gmail.com>
2025-11-29 18:01:27 +0100

    1

--- pages/equipos.php
+++ pages/equipos.php
@@ -8,7 +8,7 @@ include '../app/listar_equipos.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Equipos</title>
 </head>
 <body>
 

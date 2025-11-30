ff813c13622c7da3591839d144f0b3bfd39f45be
miguel <miguel2006ngl@gmail.com>
2025-11-30 21:33:57 +0100

    error de conexion corregido

--- app/conexion.php
+++ app/conexion.php
@@ -2,7 +2,7 @@
 $intentos = 3;
 while ($intentos > 0) {
     try {
-        $pdo = new PDO("mysql:host=localhost;port=3306;dbname=fantasy", "admin", "1234");
+        $pdo = new PDO("mysql:host=localhost;port=3306;dbname=fantasy", "root", "");
         break;
     } catch (PDOException $e) {
         sleep(2); // espera 2 segundos

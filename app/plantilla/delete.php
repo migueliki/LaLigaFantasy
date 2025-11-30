18129f11c3d3d56a5f30f376c863b408de3abd1f
miguel <miguel2006ngl@gmail.com>
2025-11-30 23:21:13 +0100

    delete sin funcionar

--- app/plantilla/delete.php
+++ app/plantilla/delete.php
@@ -1,4 +1,28 @@
 <?php
 
+// ELIMINAR JUGADOR
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $ID = $_POST['ID'] ?? '';
+
+    $sql = "DELETE FROM plantilla";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([
+        ':nombre' => $nombre,
+        ':equipo' => $equipo,
+        ':posicion' => $posicion,
+        ':nacionalidad' => $nacionalidad,
+        ':dorsal' => $dorsal
+    ]);
+
+    header("Location: plantilla.php");
+    exit();
+
+    if ($sql==1) {
+        echo '<div>Jugador eliminado de la plantilla</div>'
+    } 
+    else {
+        echo '<div>No se pudo eliminar al jugador de la plantilla</div>'
+    }
+}
 
 ?>

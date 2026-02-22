9857b3e6a73c1540749a5b9103ed10de4db052ac
miguel <miguel2006ngl@gmail.com>
2026-02-22 21:00:20 +0100

    cambios bd

--- app/conexion.php
+++ app/conexion.php
@@ -3,7 +3,7 @@
 $intentos = 3;
 while ($intentos > 0) {
     try {
-        $pdo = new PDO("mysql:host=localhost;port=3306;dbname=fantasy", "root", "");
+        $pdo = new PDO("mysql:host=localhost;port=3306;dbname=fantasy", "admin", "fantasyASIR25");
         break;
     } catch (PDOException $e) {
         sleep(2); // espera 2 segundos

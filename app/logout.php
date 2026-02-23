3a34cbebd48a3d257ad91e81b0dd3cf65ea3caf7
miguel <miguel2006ngl@gmail.com>
2026-02-23 23:04:21 +0100

    quitar app de logout

--- app/logout.php
+++ app/logout.php
@@ -17,7 +17,7 @@ setcookie(session_name(), '', time()-3600, '/'); // eliminar cookie de sesión
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cerrando sesión...</title>
     
-    <meta http-equiv="refresh" content="3;url=/app/index.php">
+    <meta http-equiv="refresh" content="3;url=/index.php">
 
     <style>
         body {
